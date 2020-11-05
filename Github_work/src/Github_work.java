abaf17dcb3187e52b616bf1eb39ed2e0f86091a6
adler <adler@192.168.1.173>
2020-11-05 14:42:12 +0800

    1

---
+++ Github_work/src/Github_work.java
@@ -0,0 +1,20 @@
+
+public class Github_work {
+
+	public static void main(String[] args) {
+		System.out.println("How old are you?");
+		System.out.println("I am 15.");
+		System.out.println(" ");
+		System.out.println("What is your favorite sport?");
+		System.out.println("Skiing and soccer.");
+		System.out.println(" ");
+		System.out.println("Did you learn java or any computer science before?");
+		System.out.println("Not a bit.");
+		System.out.println(" ");
+		System.out.println("What is your favorite software/game?");
+		System.out.println("Call of duty.");
+		System.out.println(" ");
+
+	}
+
+}
